aeaddd37aa977cdeb0f21f6f3580c94470c958cc
Kevin Van Robbroeck <kevin.vanrobbroeck@gmail.com>
2015-09-12 16:34:21 +0200

    Added some refinements for persistence testing: - Datasets now support {null} replacements - Tests now support customization of datasets - Data schema is now recreated when not in "in memory test mode" (fixes all "strange FK violations due to no more orphaned table definitions lingering around) - Tables are now exported before every unit test (performance will suffer but clients will have more "sensible" experience)  - Datasets are now no longer cleared after b/c schema is recreated anyway (was a frequent issue with dbunit datasets)  - Integration tests are now ignored BEFORE loading their persistence context (speeds them up if they are disabled)
     These changes should allow for a more smooth experience

--- ejb-module/src/test/java/com/realdolmen/course/persistence/DataSetPersistenceTest.java
+++ ejb-module/src/test/java/com/realdolmen/course/persistence/DataSetPersistenceTest.java
@@ -3,13 +3,20 @@ package com.realdolmen.course.persistence;
 import org.dbunit.database.DatabaseConfig;
 import org.dbunit.database.DatabaseConnection;
 import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.CompositeDataSet;
+import org.dbunit.dataset.DataSetException;
 import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.ReplacementDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 import org.dbunit.ext.mysql.MySqlDataTypeFactory;
 import org.dbunit.operation.DatabaseOperation;
 import org.junit.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.util.Arrays;
+import java.util.HashMap;
 
 /**
  * Loads a DBUnit test set before every unit test.
@@ -19,12 +26,40 @@ public abstract class DataSetPersistenceTest extends PersistenceTest {
 
     @Before
     public void loadDataSet() throws Exception {
-        logger.info("Loading dataset");
-        IDataSet dataSet = new FlatXmlDataSetBuilder().build(getClass().getResource("/data.xml"));
+        logger.info("Loading datasets");
+        try(Connection jdbcConnection = newConnection()) {
+            IDatabaseConnection connection = new DatabaseConnection(jdbcConnection);
+            if(DatabaseEngine.current() == DatabaseEngine.mysql) {
+                connection.getConfig().setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new MySqlDataTypeFactory()); // Set factorytype in dbconfig to remove warning
+            }
+            DatabaseOperation.INSERT.execute(connection, createDataSet());
+        }
+    }
 
-        IDatabaseConnection connection = new DatabaseConnection(newConnection());
-        connection.getConfig().setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new MySqlDataTypeFactory()); // Set factorytype in dbconfig to remove warning
-        DatabaseOperation.CLEAN_INSERT.execute(connection, dataSet);
-        connection.close();
+    /**
+     * Builds a combination dataset, including replacing {null} by null.
+     * @return The combo dataset that should be executed.
+     * @throws DataSetException When the stars are in the wrong position.
+     */
+    private IDataSet createDataSet() throws DataSetException {
+        final FlatXmlDataSetBuilder dataSetBuilder = new FlatXmlDataSetBuilder();
+        HashMap<String, Object> replacements = new HashMap<>();
+        replacements.put("{null}", null);
+        return new ReplacementDataSet(new CompositeDataSet(Arrays.stream(dataSets()).<IDataSet>map(s -> {
+            try {
+                return dataSetBuilder.build(getClass().getResource(s));
+            } catch (DataSetException e) {
+                throw new RuntimeException("Problem loading DBUnit dataset", e);
+            }
+        }).toArray(IDataSet[]::new)), replacements, null);
+    }
+
+    /**
+     * Provides a list of datasets that should be loaded.
+     * Subclasses can override this to provide customization.
+     * @return An array of Strings pointing to class-path relative dataset XML files.
+     */
+    protected String[] dataSets() {
+        return new String[] { "/data.xml" };
     }
 }
